f60a6de6d3b6dce9a8efaa1ff8082e0d35a234eb
shuyang hao <70073970+s24hao@users.noreply.github.com>
2023-01-08 12:33:46 -0500

    course content

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,13 +1,32 @@
 let js = "really amazing";
-if (js === "amazing") alert("Javascript is FUN!");
+if (js === "amazing") alert('Javascript is FUN!'); //strings can be in single or double quotes
 
 40 + 8 + 23 - 10;
 console.log(40 + 8 + 23 - 10);
+
+/*
+javascript automatically determines the datatype.(dynamic typing)
+datatype of a var can change automatically
+*/
 
 let firstName = "Matilda"; //Camel case,JS convention
 let PI = 3.14; //const in capital
 
 // _ and $ are allow in identifier
-let _function = 27;
-let $function = 27;
-console.log(firstName);
+let _function = 27.7;
+let $function;
+let funct = true;
+
+console.log(typeof funct); //determines the type of variables
+funct = "TRUE";
+console.log(typeof funct); //dynamic typing
+
+console.log(typeof 'Jonas');//type string
+console.log(typeof 23);//type number
+
+console.log($function); //undefined
+console.log(typeof $function);//type: undefined
+console.log(typeof null); //!!!!!!!!!!!! IS ACTUALLY object, common bug
+
+
+
