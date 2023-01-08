23099caa5ec2f321a9733b04811f245395d3433d
shuyang hao <70073970+s24hao@users.noreply.github.com>
2023-01-08 12:21:28 -0500

    course content

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,13 @@
+let js = "really amazing";
+if (js === "amazing") alert("Javascript is FUN!");
+
+40 + 8 + 23 - 10;
+console.log(40 + 8 + 23 - 10);
+
+let firstName = "Matilda"; //Camel case,JS convention
+let PI = 3.14; //const in capital
+
+// _ and $ are allow in identifier
+let _function = 27;
+let $function = 27;
+console.log(firstName);
